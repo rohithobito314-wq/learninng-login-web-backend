fb078d94929ad81c17cad6d6480f8483cefcfd92
ROHITH S <rohithobito314@gmail.com>
2026-06-18 08:21:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,8 @@ app.post('/login',(req,res)=>{
  if(user.password!==password) return res.json({message:'Invalid password'});
  return res.json({message:'Welcome Rohith!'});
 });
-app.listen(3000,()=>console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
